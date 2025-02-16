b5fd1fa68a812ba5f4651cccf0c04c7b619b79e8
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2025-02-16 16:54:07 +0000

    Fixed - iOS font issue

--- app/screens/login/index.tsx
+++ app/screens/login/index.tsx
@@ -3,15 +3,22 @@ import {StyleSheet} from 'react-native';
 
 // components
 import {AppContainer, AppText} from '../../components';
+import {Constants} from '../../utils';
 
 const LoginScreen = () => {
   return (
     <AppContainer>
-      <AppText text="Login screen" />
+      <AppText text="WashEase" textStyle={styles.title} />
     </AppContainer>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: Constants.FONTS.Bold,
+    fontSize: 33,
+    marginBottom: Constants.SPACES.large,
+  },
+});
 
 export default LoginScreen;
